6cb5c0b0d0696306166c564bbfdfb712f5932bda
Vajda Máté Pál <vajdamate0204@gmail.com>
2023-11-05 22:34:18 +0100

    IMG-7 feat: Create landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,21 @@
-<h1>Landing page</h1>
+<script lang="ts">
+    import { Button } from '$components/ui/button'
+    import Centered from '$lib/Centered.svelte'
 
+    import logo from '$lib/assets/icons/logo_color.png'
+</script>
+
+<Centered>
+    <img class="w-48 lg:w-1/4" src={logo} alt="Logo">
+    <h1
+    class="text-4xl lg:text-8xl uppercase mt-10 mb-5 text-slate-300">
+        unideb.tech
+    </h1>
+    <h2 class="text-l lg:text-4xl text-slate-300 mb-10"> The Best Image hosting service</h2>
+    <Button 
+        class="text-xl lg:text-4xl text-slate-400" 
+        href="/dash/login" 
+        variant="link">
+        Login
+    </Button>
+</Centered>
