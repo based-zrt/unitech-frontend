7913b3306795aadf0289ba5647377f29f039395a
Vajda Máté Pál <vajdamate0204@gmail.com>
2023-11-12 23:26:40 +0100

    IMG-16 feat: Create skeleton/first version of login page

--- src/routes/dash/login/+page.svelte
+++ src/routes/dash/login/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Dashboard Login</h1>
+<script lang="ts">
+    import * as Form from '$lib/components/ui/form';
+    import * as Card from "$lib/components/ui/card";
+    import type { formSchema, FormSchema } from './loginSchema'
+    import type { SuperValidated } from 'sveltekit-superforms';
+    export let form: SuperValidated<FormSchema>;
+
+    import LoginForm from './loginForm.svelte';
+    import Centered from '$lib/Centered.svelte'
+    import logo from '$lib/assets/icons/logo_color.png'
+
+</script>
+
+<Centered> 
+  <img class="w-48 lg:w-1/4 mb-10" src={logo} alt="Logo">
+  <Card.Root>
+    <Card.Header>
+      <Card.Title class="text-2xl">Sign in</Card.Title>
+    </Card.Header>
+    
+    <Card.Content>
+      <LoginForm  {form}></LoginForm>
+    </Card.Content>
+  </Card.Root>
+</Centered>
+
 
---
+++ src/routes/dash/login/loginForm.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import * as Form from "$lib/components/ui/form";
+    import { formSchema, type FormSchema } from "./loginSchema";
+    import type { SuperValidated } from "sveltekit-superforms";
+    export let form: SuperValidated<FormSchema>;
+</script>
+
+<Form.Root {form} schema={formSchema} let:config class="flex flex-col items-center">
+    <Form.Field {config} name="username">
+      <Form.Item>
+        <Form.Label class="text-xl" >Username</Form.Label>
+        <Form.Input />
+        <Form.Description></Form.Description>
+        <Form.Validation />
+      </Form.Item>
+    </Form.Field>
+
+    <Form.Field {config} name="password">
+        <Form.Item>
+          <Form.Label class="text-xl">Password</Form.Label>
+          <Form.Input />
+          <Form.Description></Form.Description>
+          <Form.Validation />
+        </Form.Item>
+    </Form.Field>
+
+    <Form.Button>Login</Form.Button>
+    <Form.Button variant="link" href="/dash/register">Register</Form.Button>
+
+    
+    
+    
+  </Form.Root>
