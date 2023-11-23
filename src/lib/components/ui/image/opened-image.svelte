342741c9e617cecaf5a1a888dbdbf7f50e6c017e
Verebik Alex <alexverebik123@gmail.com>
2023-11-23 20:09:45 +0100

    refactor Dashboard: Add simple data structure

---
+++ src/lib/components/ui/image/opened-image.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+import Download from '$components/downloadbutton/download.svelte'
+import baseImage from '$lib/assets/images/unknownImage.jpg';
+
+export let imageData = {
+    imageId: undefined,
+    imageName: undefined,
+    imageSize: undefined,
+    imageUploadDate: undefined,
+    imageSource: undefined,
+    imageLink: undefined,
+};
+ 
+//function generateImageLink(){}
+</script>
+
+<div class="cursor-pointer w-120 h-120 border-2 border-black mb-10">
+    {#if imageData.imageSource}
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img class="h-fit w-fit rounded-lg shadow-xl dark:shadow-gray-800" src={imageData.imageSource} alt="Image">
+        <div class="h-100 border-4 border-white">
+            <span>Image name: {imageData.imageName}</span>
+            <span>Image size: {imageData.imageSize}</span>
+            <span>Image upload date: {imageData.imageUploadDate}</span>
+        </div>
+    {:else}
+        <!-- svelte-ignore a11y-img-redundant-alt -->
+        <img class="h-fit w-fit rounded-lg shadow-xl dark:shadow-gray-800" src={baseImage} alt="Default Image">
+        <div class="h-100 border-2 border-dotted mt-2 border-white p-4">
+            <span>Name: {imageData.imageName} <br></span>
+            <span>Size: {imageData.imageSize} <br></span>
+            <span>Uploaded: {imageData.imageUploadDate} <br></span>
+        </div>
+        <div class="h-fit w-fit mx-auto mt-5"><Download /></div>
+    {/if}
+</div>
--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -6,51 +6,51 @@ import deffaultProfile from '$lib/assets/images/deffaultProfile.png';
 import { Progress } from "$lib/components/ui/progress";
 import Uploadbutton from '$components/ui/uploadbutton/uploadbutton.svelte';
 import * as Dialog from "$lib/components/ui/dialog";
-import * as Image from '$components/ui/image-container';
-
+import Imagecontainer from '$components/ui/image/imagecontainer.svelte';
+import OpenedImage from '$components/ui/image/opened-image.svelte';
 
 //Profile varriables - these are got from database.
-let imgcount = 10; 
-let profileImage; 
-let maxStorage=0.8;
-let usedStorage=500;
+let user = {
+  userId: undefined,
+  userName: 'Jane Doe',
+  profileImage: undefined,
+  imageCount: '10',
+  maxStorageSize: '0.8',
+  usedStorage: '500'
+};
 
-//function loadImage(){} - Image get from database
+//function loadImage(){} - Image get from database and this goes for the imageData.imageSource.
 
 </script>
-<!--TODO: Add responsivity to page.-->
 
 <!--Navbar-->
-<header>
-  <nav class="fixed bg-slate-800 w-full flex justify-between items-center mx-auto px-8 h-20">
+<nav class="fixed bg-slate-800 w-full flex justify-between items-center mx-auto px-8 h-20">
     <Logo />
-    <Progress storage={maxStorage} used={usedStorage} /> <!--The storage always should be add as GB but the used always MB. -->
+    <Progress storage={user.maxStorageSize} used={user.usedStorage} /> <!--The 'storage' always should be add as GB but the 'used' always as MB. -->
     <Searchbar />
     <Uploadbutton />
     <Avatar.Root>
         <Avatar.Image src={deffaultProfile} alt="@shadcn" />
         <Avatar.Fallback>CN</Avatar.Fallback>
       </Avatar.Root>
-    </nav>
-</header>
+</nav>
 <!--Navbar-->
 
 <!--Body-->
 <div class="pt-32 pl-10">
   <div class="grid lg:grid-cols-5 gap-2 md:grid-cols-3 sm:grid-cols-2 ml-11 lg:ml-0">
-    {#each Array.from({ length: imgcount }) as _, i (i)}
+    {#each Array.from({ length: user.imageCount }) as _, i (i)}
     <Dialog.Root>
-      <Dialog.Trigger><Image.Imagecontainer srcImage=""/></Dialog.Trigger>
+      <Dialog.Trigger><Imagecontainer bind:imageData.imageSource=""/></Dialog.Trigger>
       <Dialog.Content>
         <Dialog.Header>
           <Dialog.Title class="text-center pt-5 pb-5">Preview</Dialog.Title>
           <Dialog.Description>
-            <Image.OpenedImage srcImage="" imageName="" imageSize="" imageUploadDate=""/>
+            <OpenedImage bind:imageData.imageSource="" imageData.imageName="" imageData.imageSize="" imageData.imageUploadDate=""/>
           </Dialog.Description>
         </Dialog.Header>
       </Dialog.Content>
     </Dialog.Root>
-    
     {/each}
   </div>
 </div>
