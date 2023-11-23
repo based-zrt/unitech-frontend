7a3948aa29b60535f3b04b4da618968453a36013
Verebik Alex <alexverebik123@gmail.com>
2023-11-23 10:19:46 +0100

    feat: Add navbar to dashboard

--- src/routes/dash/+page.svelte
+++ src/routes/dash/+page.svelte
@@ -1,1 +1,18 @@
-<h1>Dashboard</h1>
+<script lang="ts">
+import * as Avatar from '$components/ui/avatar';
+import Logo from '$components/ui/logo/logo.svelte'
+import Searchbar from '$components/ui/searchbar/searchbar.svelte';
+import deffaultProfile from '$lib/assets/images/deffaultProfile.png';
+
+
+</script>
+
+<nav class=" bg-slate-800 w-full flex relative justify-between items-center mx-auto px-8 h-20">
+<Logo />
+<Searchbar />
+<Avatar.Root>
+    <Avatar.Image src={deffaultProfile} alt="@shadcn" />
+    <Avatar.Fallback>CN</Avatar.Fallback>
+  </Avatar.Root>
+  
+</nav>
