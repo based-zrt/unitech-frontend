706e57306ace83d629b71a7ede0ac6a33f366e31
Verebik Alex <alexverebik123@gmail.com>
2023-11-23 14:10:46 +0100

    feat Dashboard: Add image cointainer.

---
+++ src/lib/components/ui/image-container/imagecontainer.svelte
@@ -0,0 +1,12 @@
+<script>
+    import baseImage from '$lib/assets/images/unknownImage.jpg';
+    export let srcImage;
+</script>
+
+<div class="cursor-pointer w-60 h-60 border-2 border-black mb-10">
+    {#if srcImage}
+        <img class="h-auto rounded-lg shadow-xl dark:shadow-gray-800" src={srcImage} alt="Image">
+    {:else}
+        <img class="h-auto rounded-lg shadow-xl dark:shadow-gray-800" src={baseImage} alt="Default Image">
+    {/if}
+</div>
