f76b886f3572d870be8689bcab0490fae902d0af
MrExplode <me@sunstorm.rocks>
2023-11-29 12:17:54 +0100

    feat: Add title to navbar

--- src/lib/components/dashboard/DashLogo.svelte
+++ src/lib/components/dashboard/DashLogo.svelte
@@ -4,8 +4,9 @@
 
 <div class="inline-flex">
     <a class="_o6689fn" href="/dash"
-        ><div class="hidden md:block">
+        ><div class="flex flex-row items-center">
             <img src={logo} alt="Logo" class="w-16" />
+            <p class="text-2xl uppercase">unideb.tech</p>
         </div>
     </a>
 </div>
